c84f9012a745abd81d0661b081ca51b992491149
Dmytro Morozov <puny.flash@gmail.com>
2025-05-11 19:36:06 +0300

    [auto] update facade methods

--- src/Notifications/TelegramNotification.php
+++ src/Notifications/TelegramNotification.php
@@ -1244,7 +1244,7 @@ namespace WeStacks\TeleBot\Laravel\Notifications;
  * - _int_ `$message_id` __Required: Yes__. Unique identifier of the message to mark as read
  *
  *
- * @method static self deleteBusinessMessages(...$parameters) Delete messages on behalf of a business account. Requires the can_delete_outgoing_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
+ * @method static self deleteBusinessMessages(...$parameters) Delete messages on behalf of a business account. Requires the can_delete_sent_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
  *
  * {@see https://core.telegram.org/bots/api#deletebusinessmessages}
  *
